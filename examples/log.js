76b7bc12ebae556914c4fb84db0a6d3ae2229c58
JustinChowBeliefChain <zhouzhiyu@beliefchain.com>
2018-06-19 23:20:42 +0800

    lint code, add [] around line number

--- examples/log.js
+++ examples/log.js
@@ -4,16 +4,25 @@ Logger.configure(Config, { serverId: process.pid });
 
 const rpcLog = Logger.getLogger("rpc-log", __filename, process.pid);
 const rpcDebug = Logger.getLogger("rpc-debug", __filename, process.pid);
+const adminLog = Logger.getLogger("admin", process.pid, __filename);
 const logger = Logger.getLogger();
 
+// This Is Same As `lineDebug = true` In Config.
 process.env.LOGGER_LINE = true;
 
-rpcLog.info("test1");
-rpcLog.warn("test2");
-rpcLog.error("test3");
+rpcLog.debug("RPC Log - debug");
+rpcLog.info("RPC Log - info");
+rpcLog.warn("RPC Log - warn");
+rpcLog.error("RPC Log - error");
 
-rpcDebug.info("test1");
-rpcDebug.warn("test2");
-rpcDebug.error("test3");
+rpcDebug.debug("RPC Debug Log - debug");
+rpcDebug.info("RPC Debug Log - info");
+rpcDebug.warn("RPC Debug Log - warn");
+rpcDebug.error("RPC Debug Log - error");
 
-logger.debug("aaa");
+adminLog.debug("Admin Log - debug");
+adminLog.info("Admin Log - info");
+adminLog.warn("Admin Log - warn");
+adminLog.error("Admin Log - error");
+
+logger.debug("Default Log With No Category. Not Suggested");
